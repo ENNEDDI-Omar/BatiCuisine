e4cfc573c872d54ee564bc63e2629a9c2cb95fa8
ENNEDDI-Omar <enneddiomar@gmail.com>
2024-09-22 11:46:36 +0200

    integration d'exception et validation pour le client.

---
+++ src/startup/exceptions/ClientNotFoundException.java
@@ -0,0 +1,7 @@
+package startup.exceptions;
+
+public class ClientNotFoundException extends RuntimeException {
+    public ClientNotFoundException(String message) {
+        super(message);
+    }
+}
--- src/startup/utils/Validations.java
+++ src/startup/utils/Validations.java
@@ -1,4 +1,23 @@
 package startup.utils;
 
+import startup.domain.entities.Client;
+import startup.exceptions.ClientNotFoundException;
+
 public class Validations {
+   public static void clientValidation(Client client)
+   {
+       if (client.getName() == null || client.getName().trim().isEmpty())
+       {
+           throw new ClientNotFoundException("Client cannot be empty");
+       }if (client.getAddress() == null || client.getAddress().trim().isEmpty())
+       {
+           throw new ClientNotFoundException("Address cannot be empty");
+       }if (client.getPhone() == null || client.getPhone().trim().isEmpty())
+       {
+              throw new ClientNotFoundException("Phone cannot be empty");
+       }if (client.getPhone().length() > 20)
+       {
+           throw new ClientNotFoundException("Phone cannot be longer than 20 characters");
+       }
+   }
 }
